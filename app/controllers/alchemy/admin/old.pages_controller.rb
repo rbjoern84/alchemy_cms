8f4020f12a534c17fc378ea513b3fd2c86f5735a
Thomas von Deyen <tvdeyen@gmail.com>
2014-06-30 10:47:17 +0200

    brainstorming nodes.
    # Conflicts:
    #	app/controllers/alchemy/admin/pages_controller.rb
    #	config/routes.rb

--- app/controllers/alchemy/admin/old.pages_controller.rb
+++ app/controllers/alchemy/admin/old.pages_controller.rb
@@ -9,13 +9,18 @@ module Alchemy
       before_action :load_page,
         only: [:show, :info, :unlock, :visit, :publish, :configure, :edit, :update, :destroy, :fold]
 
+      before_action :set_root_page,
+        only: [:index, :show, :sort, :order]
+
       authorize_resource class: Alchemy::Page
 
-      # Lists all pages
-      #
       def index
         @locked_pages = Page.from_current_site.all_locked_by(current_alchemy_user)
-        @pages = Page.all.where(language_id: Language.current.id)
+        @languages = Language.all
+        if !@page_root
+          @language = Language.current
+          @languages_with_page_tree = Language.with_root_page
+        end
       end
 
       # Used by page preview iframe in Page#edit view.
@@ -28,8 +33,6 @@ module Alchemy
         render layout: 'application'
       end
 
-      # Displays information of this page
-      #
       def info
         render layout: !request.xhr?
       end
@@ -148,7 +151,7 @@ module Alchemy
 
       def visit
         @page.unlock!
-        redirect_to show_page_path(urlname: @page.urlname, locale: multi_language? ? @page.language_code : nil)
+        redirect_to show_page_path(:urlname => @page.urlname, :lang => multi_language? ? @page.language_code : nil)
       end
 
       # Sets the page public and updates the published_at attribute that is used as cache_key
@@ -158,6 +161,33 @@ module Alchemy
         @page.publish!
         flash[:notice] = _t(:page_published, :name => @page.name)
         redirect_back_or_to_default(admin_pages_path)
+      end
+
+      def copy_language_tree
+        language_root_to_copy_from.copy_children_to(copy_of_language_root)
+        flash[:notice] = _t(:language_pages_copied)
+        redirect_to admin_pages_path
+      end
+
+      def sort
+        @sorting = true
+      end
+
+      # Receives a JSON object representing a language tree to be ordered
+      # and updates all pages in that language structure to their correct indexes
+      def order
+        neworder = JSON.parse(params[:set])
+        tree = create_tree(neworder, @page_root)
+
+        Alchemy::Page.transaction do
+          tree.each do |key, node|
+            dbitem = Page.find(key)
+            dbitem.update_node!(node)
+          end
+        end
+
+        flash[:notice] = _t("Pages order saved")
+        do_redirect_to admin_pages_path
       end
 
       def switch_language
@@ -176,8 +206,105 @@ module Alchemy
 
       private
 
+      def copy_of_language_root
+        page_copy = Page.copy(
+          language_root_to_copy_from,
+          language_id: params[:languages][:new_lang_id],
+          language_code: Language.current.code
+        )
+        page_copy.move_to_child_of Page.root
+        page_copy
+      end
+
+      def language_root_to_copy_from
+        Page.language_root_for(params[:languages][:old_lang_id])
+      end
+
+      # Returns the current left index and the aggregated hash of tree nodes indexed by page id visited so far
+      #
+      # Visits a batch of children nodes, assigns them the correct ordering indexes and spuns recursively the same
+      # procedure on their children, if any
+      #
+      # @param [Array]
+      #   An array of children nodes to be visited
+      # @param [Integer]
+      #   The lft attribute that should be given to the first node in the array
+      # @param [Integer]
+      #   The page id of the parent of this batch of children nodes
+      # @param [Integer]
+      #   The depth at which these children reside
+      # @param [Hash]
+      #   A Hash of TreeNode's indexed by their page ids
+      # @param [String]
+      #   The url for the parent node of these children
+      # @param [Boolean]
+      #   Whether these children reside in a restricted branch according to their ancestors
+      #
+      def visit_nodes(nodes, my_left, parent, depth, tree, url, restricted)
+        nodes.each do |item|
+          my_right = my_left + 1
+          my_restricted = item['restricted'] || restricted
+          urls = process_url(url, item)
+
+          if item['children']
+            my_right, tree = visit_nodes(item['children'], my_left + 1, item['id'], depth + 1, tree, urls[:children_path], my_restricted)
+          end
+
+          tree[item['id']] = TreeNode.new(my_left, my_right, parent, depth, urls[:my_urlname], my_restricted)
+          my_left = my_right + 1
+        end
+
+        [my_left, tree]
+      end
+
+      # Returns a Hash of TreeNode's indexed by their page ids
+      #
+      # Grabs the array representing a tree structure of pages passed as a parameter,
+      # visits it and creates a map of TreeNodes indexed by page id featuring Nested Set
+      # ordering information consisting of the left, right, depth and parent_id indexes as
+      # well as a node's url and restricted status
+      #
+      # @param [Array]
+      #   An Array representing a tree of Alchemy::Page's
+      # @param [Alchemy::Page]
+      #   The root page for the language being ordered
+      #
+      def create_tree(items, rootpage)
+        _, tree = visit_nodes(items, rootpage.lft + 1, rootpage.id, rootpage.depth + 1, {}, "", rootpage.restricted)
+        tree
+      end
+
+      # Returns a pair, the path that a given tree node should take, and the path its children should take
+      #
+      # This function will add a node's own slug into their ancestor's path
+      # in order to create the full URL of a node
+      #
+      # NOTE: external and invisible pages are not part of the full path of their children
+      #
+      # @param [String]
+      #   The node's ancestors path
+      # @param [Hash]
+      #   A children node
+      #
+      def process_url(ancestors_path, item)
+        default_urlname = (ancestors_path.blank? ? "" : "#{ancestors_path}/") + item['slug'].to_s
+
+        pair = {my_urlname: default_urlname, children_path: default_urlname}
+
+        if item['external'] == true || item['visible'] == false
+          # children ignore an ancestor in their path if external or invisible
+          pair[:children_path] = ancestors_path
+        end
+
+        pair
+      end
+
       def load_page
         @page = Page.find(params[:id])
+      end
+
+      def pages_from_raw_request
+        request.raw_post.split('&').map { |i| i = {i.split('=')[0].gsub(/[^0-9]/, '') => i.split('=')[1]} }
       end
 
       def redirect_path_for_switch_language
@@ -219,6 +346,10 @@ module Alchemy
           parent = Page.find_by(id: params[:page][:parent_id]) || Page.root
           Page.copy_and_paste(source, parent, params[:page][:name])
         end
+      end
+
+      def set_root_page
+        @page_root = Language.current_root_page
       end
 
     end
